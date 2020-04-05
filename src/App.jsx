8d6bfe03b13994b5f785dea65039a25e1138b92d
T. Lanette <TLPollard4516@gmail.com>
2020-04-05 17:55:08 -0400

    Added Button Component and styling
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
+import Button from './components/Button';
 
 class App extends Component {
 render() {
     return (
       <div className="app">
         <div className="calc-wrapper">
-          
+          <div className="row">
+            <Button id="seven">7</Button>
+            <Button id="eight">8</Button>
+            <Button id="nine">9</Button>
+            <Button id="divide">/</Button>
+          </div>
         </div>
       </div>
     );

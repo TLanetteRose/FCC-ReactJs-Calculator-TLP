e982a0a835df554cf5a78ff17e6e34e795a58763
T. Lanette <TLPollard4516@gmail.com>
2020-04-11 20:11:18 -0400

    Update
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/ClearButton.jsx
+++ src/components/ClearButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ClearButton.css';
 
 export const ClearButton = (props) => (
-         <div id={props.id} className="clear-btn" onClick={props.clearDisplay}>
+         <div id={props.id} className="clear-btn">
            {props.children}
          </div>
        );
